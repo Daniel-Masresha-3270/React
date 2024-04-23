1440cd39030936971687795060ce2f03fa335c52
Daniel-Masresha-3270 <danielmasresha04@gmail.com>
2024-04-23 17:03:29 +0300

    new file Student.jsx is added changes to App.jsx and index.css

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,12 +1,14 @@
-import Card from "./card"
+import Student from './Student.jsx'
 
 function App() {
  return (
   <>
-    <Card />
-    <Card />
+    <Student name="Adane" age="18" isStudent= {true}/>
+    <Student name="Amir" age={19} isStudent= {true}/>
+    <Student name="Eyob" age={25} isStudent= {false}/>
+    <Student />
   </>
- )
+ );
 }
 
 export default App
