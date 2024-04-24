5ba86412591ca47042dc723517d726bd56d53e72
Daniel-Masresha-3270 <danielmasresha04@gmail.com>
2024-04-24 08:14:28 +0300

    modified:   my-first-app/src/App.jsx

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -3,7 +3,7 @@ import UserGreeting from './UserGreeting.jsx'
 function App() {
  return (
   <>
-    <UserGreeting username= "Samuel" isLoggedIn= {false}/>
+    <UserGreeting username= "Samuel" isLoggedIn= {true}/>
   </>
  );
 }
