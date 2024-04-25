c7a167ddc8ec18388cdc864216ead41c655577ee
Daniel-Masresha-3270 <danielmasresha04@gmail.com>
2024-04-25 23:21:59 +0300

    some changes to List.jsx

--- my-first-app/src/List.jsx
+++ my-first-app/src/List.jsx
@@ -5,6 +5,15 @@ function List(){
                     {id: 3, name: "orange", calories: 45}, 
                     {id: 4, name: "coconut", calories: 159}, 
                     {id: 5, name: "pineapple", calories: 37}];
+    
+    // fruits.sort((a, b) => a.name.localeCompare(b.name));  // ALPHABETICAL
+    //fruits.sort((a, b) => b.name.localeCompare(a.name));  //REVERSE ALPHABETICAL
+    // fruits.sort((a, b) => a.calories - b.calories);      //numeric
+    fruits.sort((a, b) => b.calories - a.calories);        //reverse numeric
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100); // filter
+    const highCalFruits = fruits.filter(highCalFruit => highCalFruit.calories >= 100)
+
     const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp;
                                                              <b>{fruit.calories}</b></li>);
     return(
